08a19ee82911eff8be927d27476cdb9245c2ed2c
kidistca <kidistca@gmail.com>
2019-10-02 18:59:44 +0100

    Profile picture

--- client/src/views/SignUp.jsx
+++ client/src/views/SignUp.jsx
@@ -3,7 +3,7 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 
-import { SignUp as signUpService } from "./../services/auth-api";
+import { signUp as signUpService } from "./../services/auth-api";
 
 export default class SignUp extends Component {
   constructor(props) {
---
+++ controllers/auth/uploadprofilepic.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const User = require("./../../models/user/user");
+
+module.exports = (req, res, next) => {
+  const { url } = req.file;
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      ...User(url && { image: url })
+    },
+    { new: true }
+  )
+    .then(user => {
+      if (!user) {
+        next(new Error("user_not_found"));
+        return;
+      }
+      res.json({ user });
+    })
+    .catch(error => {
+      next(error);
+    });
+};
