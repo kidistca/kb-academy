0e72e28befb3ef416892fa86c6ed35a70d8a775c
kidistca <kidistca@gmail.com>
2019-10-02 12:41:02 +0100

    routed defined

---
+++ controllers/auth/signup.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const User = require("./../../models/user/user");
+
+module.exports = (req, res, next) => {
+  const { name, email, password } = req.body;
+  User.signUp({ name, email, password })
+    .then(user => {
+      req.session.user = {
+        _id: user._id
+      };
+      req.json({ user });
+    })
+    .catch(error => {
+      next(error);
+      console.log(error);
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,4 +3,16 @@
 const { Router } = require("express");
 const router = Router();
 
+const SignUpControllers = require("./../controllers/auth/signup");
+const SignInControllers = require("./../controllers/auth/signin");
+// const SignedInControllers = require("./../controllers/auth/signedin");
+// const SignOutControllers = require("./../controllers/auth/signout");
+// const EditControllers = require("./../controllers/auth/edit");
+const routeGuardMiddleware = require("./../middleware/route-guard");
+
+router.post("/auth/signup", routeGuardMiddleware(false), SignUpControllers);
+router.post("/auth/signin", routeGuardMiddleware(false), SignInControllers);
+// router.post("/auth/signout", routeGuardMiddleware(true), SignOutControllers);
+// router.post("/auth/signedIn", SignedInControllers);
+
 module.exports = router;
