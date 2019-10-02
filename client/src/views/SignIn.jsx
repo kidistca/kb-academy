e614dc3ce2f4e6bd6570c7eaa7b2504bdb277c89
kidistca <kidistca@gmail.com>
2019-10-02 12:54:48 +0100

    signin & signup views created

---
+++ client/src/views/SignIn.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+import { signin as loginService } from "./../services/auth-api";
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmitForm = this.onSubmitForm.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  //Function passed as prop from app
+  //this.props.loadUser(user);
+
+  onSubmitForm(event) {
+    event.preventDefault();
+    const { email, password } = this.state;
+    loginService({ email, password })
+      .then(user => {
+        this.props.history.push("/profile");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <Container>
+        <h1>Login</h1>
+        <Form onSubmit={this.onSubmitForm}>
+          <Form.Group>
+            <Form.Label htmlFor="user-email">Email</Form.Label>
+            <Form.Control
+              id="user-email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="user-password">Password</Form.Label>
+            <Form.Control
+              id="user-password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Button type="submit">Sign in</Button>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+//client - services- authentication axios,
+//        views - forms with handle changes
+//server - controllers -router contrroller
+// routes - login
