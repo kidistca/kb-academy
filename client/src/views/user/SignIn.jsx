72821add1b57a0f5a7c65c010c79e770a0127b2a
beneditaalmeida <benedita@MBP-de-Benedita.lan>
2019-10-06 21:23:00 +0100

    create exercise Math

--- client/src/views/user/SignIn.jsx
+++ client/src/views/user/SignIn.jsx
@@ -42,56 +42,51 @@ export default class Login extends Component {
 
   render() {
     return (
-      <Container className="d-flex justify-content-center">      
-            <Card
-              bg="transparent"
-              text="info"
-              border="info"
-              className="my-5  px-3"
-            >
-              <Card.Body>
-                <h1 className="my-3 font-weight-lighter">
-                  Find a challenge for you!
-                </h1>
-                <Form onSubmit={this.onSubmitForm}>
-                  <Form.Group>
-                    <Form.Label htmlFor="user-email" className="mt-3">
-                      Email
-                    </Form.Label>
-                    <Form.Control
-                      id="user-email"
-                      name="email"
-                      type="email"
-                      placeholder="Email"
-                      value={this.state.email}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label htmlFor="user-password" className="mt-3">
-                      Password
-                    </Form.Label>
-                    <Form.Control
-                      id="user-password"
-                      name="password"
-                      type="password"
-                      placeholder="Password"
-                      value={this.state.password}
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                  <Button type="submit" className="mt-3" variant="info">
-                    Sign in
-                  </Button>
-                  <p className="text-info mt-5 mb-3">
-                    New in Edukids?{" "}
-                    <a href="/signup" className="text-info">
-                      Sign Up here.
-                    </a>
-                  </p>
-                </Form>
-              </Card.Body>
-            </Card>
+      <Container className="d-flex justify-content-center">
+        <Card bg="transparent" text="info" border="info" className="my-5  px-3">
+          <Card.Body>
+            <h1 className="my-3 font-weight-lighter">
+              Find a challenge for you!
+            </h1>
+            <Form onSubmit={this.onSubmitForm}>
+              <Form.Group>
+                <Form.Label htmlFor="user-email" className="mt-3">
+                  Email
+                </Form.Label>
+                <Form.Control
+                  id="user-email"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  value={this.state.email}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Form.Group>
+                <Form.Label htmlFor="user-password" className="mt-3">
+                  Password
+                </Form.Label>
+                <Form.Control
+                  id="user-password"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button type="submit" className="mt-3" variant="info">
+                Sign in
+              </Button>
+              <p className="text-info mt-5 mb-3">
+                New in Edukids?{" "}
+                <a href="/signup" className="text-info">
+                  Sign Up here.
+                </a>
+              </p>
+            </Form>
+          </Card.Body>
+        </Card>
       </Container>
     );
   }
